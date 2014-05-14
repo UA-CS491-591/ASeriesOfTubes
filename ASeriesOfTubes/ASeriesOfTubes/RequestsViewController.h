13d81c8fb00a9e4d9eb927943466b9fdda6ec025
Matthew York <myork@cs.ua.edu>
2014-05-14 08:33:37 -0500

    - Added UI for making a GET Request

--- ASeriesOfTubes/ASeriesOfTubes/RequestsViewController.h
+++ ASeriesOfTubes/ASeriesOfTubes/RequestsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RequestsViewController : UIViewController
+@interface RequestsViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- ASeriesOfTubes/ASeriesOfTubes/RequestsViewController.m
+++ ASeriesOfTubes/ASeriesOfTubes/RequestsViewController.m
@@ -9,6 +9,7 @@
 #import "RequestsViewController.h"
 
 @interface RequestsViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 
 @end
 
@@ -34,5 +35,26 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)didSelectGo:(id)sender {
+    //Hide keyboard
+    [self.view endEditing:YES];
+    
+    //Make request
+    [self makeRequest];
+}
 
+#pragma mark - UITextField Delegate
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    //Hide keyboard
+    [self.view endEditing:YES];
+    
+    //Make request
+    [self makeRequest];
+    
+    return YES;
+}
+
+-(void)makeRequest{
+    
+}
 @end
