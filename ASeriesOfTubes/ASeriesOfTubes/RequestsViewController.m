fc38b291bc41a97294a4fe1a9319d65ffeaa49de
CS491 <myork@cs.ua.edu>
2014-05-14 10:34:17 -0500

    - Added synchronous & asynchronous tasks - Added login

--- ASeriesOfTubes/ASeriesOfTubes/RequestsViewController.m
+++ ASeriesOfTubes/ASeriesOfTubes/RequestsViewController.m
@@ -10,7 +10,8 @@
 
 @interface RequestsViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
-
+@property (weak, nonatomic) IBOutlet UITextView *responseTextView;
+@property NSOperationQueue *operationQueue;
 @end
 
 @implementation RequestsViewController
@@ -28,6 +29,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    //Set up operation queue to handle background tasks.
+    //This will be used in asyncronous method calls
+    _operationQueue = [[NSOperationQueue alloc] init];
+    
+    //[self login];
 }
 
 - (void)didReceiveMemoryWarning
@@ -55,6 +62,84 @@
 }
 
 -(void)makeRequest{
+    //Send synchronous request
+    //[self sendSynchronousRequest];
     
+    //Send asynchronous request
+    [self sendAsynchronousRequest];
+}
+
+-(void)sendSynchronousRequest{
+    //1. Create request object
+    //Create url
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@", _urlTextField.text]];
+    
+    //Create request object
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
+    
+    //Add header
+    //[request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    //Specify verb
+    [request setHTTPMethod:@"GET"];
+    
+    //Create response variables that will be used by the synchronous request
+    NSURLResponse *response;
+    NSError *error;
+    
+    //Send synchronous request
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    //Handle response
+    NSString *responseString = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+    _responseTextView.text = responseString;
+}
+
+-(void)sendAsynchronousRequest{
+    //Create url
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@", _urlTextField.text]];
+    
+    //Create request object
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
+    
+    //Make request
+    [NSURLConnection sendAsynchronousRequest:request queue:_operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        
+        //3. Handle response
+        NSString *responseString = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+        
+        //Hop back to main thread to update UI
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _responseTextView.text = responseString;
+        });
+        
+    }];
+}
+
+-(void)login{
+    //Create url
+    NSURL *url = [NSURL URLWithString:@"https://mobileweb.caps.ua.edu/cs491/api/Account/login"];
+    
+    //Create request object
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
+    //Let the server know that we want to interact in JSON
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    //Set http method
+    [request setHTTPMethod:@"POST"];
+    
+    //Specify the string to get sent to the server
+    NSString *loginString = @"{\"username\": \"zbarnes\",\"password\": \"password\"}";
+    //Make that string into raw data
+    NSData *loginData = [loginString dataUsingEncoding:NSUTF8StringEncoding];
+    //Set that raw data as the HTTP Body for the request
+    [request setHTTPBody:loginData];
+    
+    //Send asynchronous request
+    [NSURLConnection sendAsynchronousRequest:request queue:_operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        //Decode to string
+        NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        //Log out response
+        NSLog(@"%@", responseString);
+    }];
 }
 @end
